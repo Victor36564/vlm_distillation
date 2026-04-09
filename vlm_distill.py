a7c6c70466e66e0bd4ad1885a5ac8eb457f1e99b
Victor Tishkevich <victish9@gmail.com>
2026-04-09 14:31:37 -0400

    Updated

--- vlm_distill.py
+++ vlm_distill.py
@@ -272,29 +272,5 @@ def main():
         language_model_name=model_name,
     )
 
-    # prompt = "Give me a short introduction to large language model."
-    # messages = [
-    #     {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-    #     {"role": "user", "content": prompt}
-    # ]
-    # text = language_tokenizer.apply_chat_template(
-    #     messages,
-    #     tokenize=False,
-    #     add_generation_prompt=True
-    # )
-    # model_inputs = language_tokenizer([text], return_tensors="pt").to(language_model.device)
-
-    # generated_ids = language_model.generate(
-    #     **model_inputs,
-    #     max_new_tokens=512
-    # )
-    # generated_ids = [
-    #     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-    # ]
-
-    # response = language_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-    # print(response)
-
 if __name__ == "__main__":
     main()
